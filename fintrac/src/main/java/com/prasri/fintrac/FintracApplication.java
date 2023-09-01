1e39d96222779c017e96296368f080864ef668f2
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2023-09-02 02:36:35 +0530

    Add files via upload

---
+++ fintrac/src/main/java/com/prasri/fintrac/FintracApplication.java
@@ -0,0 +1,13 @@
+package com.prasri.fintrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintracApplication.class, args);
+	}
+
+}
---
+++ fintrac/src/test/java/com/prasri/fintrac/FintracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasri.fintrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
